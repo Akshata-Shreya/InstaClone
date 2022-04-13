c419fee0b9259edd8943ce72a634260062160d37
Akshata Sangwai <akshatasangwai1234@gmail.com>
2022-04-13 19:01:41 +0530

    aws s3 connections started

--- Igclone/Igclone/settings.py
+++ Igclone/Igclone/settings.py
@@ -103,6 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#S3 configuration
+
+AWS_ACCESS_KEY_ID = 'AKIAWCPJQXB65N435KVV'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'ccl-practical-1019153'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
