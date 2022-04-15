2920eb608fd6f8f98fd3d1e37966e7ae7651e650
ShreyaSapale <shreya17sapale@gmail.com>
2022-04-15 12:14:38 +0530

    Add post in database done

--- Igclone/instagram/views.py
+++ Igclone/instagram/views.py
@@ -1,9 +1,10 @@
-import imp
+
 import uuid
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from utils import user_handle, post_handle
 import requests
+import json
 
 # Create your views here.
 def index(request):
@@ -38,49 +39,49 @@ def login(request):
 def newPost(request,userid):
     if request.method == 'POST':
         files = request.FILES
-        # filename = request.FILES['filename']
-        # for filename, file in request.FILES.iteritems():
-        #     name = request.FILES[filename].name
-        #     print(name)
+
         post = files.get('usr_post')
+        # location = request.POST['location']
+        # description = request.POST['description']
+        location = request.POST.get('location','')
+        description = request.POST.get('description','')
+
         headers = {
                     'Accept': 'application/json',
-                    'Content-Type': 'image/png'
+                    'Content-Type': 'image/png',
+                    "Accept":"*/*",
                 }
 
-        # print(type(post))
-
-        url = "https://qofpjxi1zg.execute-api.ap-south-1.amazonaws.com/v6/ccl-practical-1019153/test1"
-        r = requests.put(url=url,data=post,headers=headers)
-
-        print(r)
-        print(r.content)
-
-
-        files = request.FILES  # multivalued dict
-        print(type(files))
-        print(files)
         
-        post = files.get("image")
-        location = request.POST['location']
-        description = request.POST['description']
-        url = "https://qofpjxi1zg.execute-api.ap-south-1.amazonaws.com/v6"+"/ccl-practical-1019153/"+str(userid)
-        headers={
-            "Content-Type":"image/jpeg",
-            "User-Agent":"PostmanRuntime/7.28.4",
-            "Accept":"*/*",
-            "Accept":"application/json"
+
+        file = open('config.json')
+        config_json = json.load(file)
+        file.close()
+
+        base_url = config_json['S3-upload']
+        folder = config_json['folder']
+
+        postid = str(uuid.uuid4())
+
+        url = base_url+folder+postid
+
+        response = requests.put(url=url, headers=headers, data=post)
+        
+        print(response.text)
+
+        post_object = {
+            '_id' : postid,
+            'postID':postid,
+            'userID':userid,
+            'location':location,
+            'description':description
         }
-        response = requests.put(url=url,headers=headers ,data=post)
-        print(type(post))
-        print(location)
-        print(description)
-        # print(response.json())
-        # post_handle.insert_one(
-        #     {
-        #         '_id':str(uuid.uuid4()),
-        #         'userid':userid,
-        #         ''
-        #         }
-        #         )
-    return render(request,'newPost.html')
+
+        post_handle.insert_one(post_object)
+
+    if request.method == 'POST': 
+
+        user = user_handle.find_one({'userID':userid})  
+         
+        
+        return render(request,'newPost.html',{'userid':userid})
